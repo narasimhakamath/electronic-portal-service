53a01115411d3112c5fe2615ea599ec859b17bc0
Narasimha Kamath <narasimhakamath@outlook.com>
2022-02-15 10:21:10 +0530

    - Installed qrcode and redis. - Created a component: einvoice.js to manage the headers and token. - Created a new utility function: cacheAuthenticationToken() to save the access token to the cache with an expiry duration.

--- src/utilities.js
+++ src/utilities.js
@@ -1,10 +1,22 @@
 const QRCode = require('qrcode');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+    host: 'localhost',
+    port: 6379
+});
 
 const convertJWTToBuffer = async (jsonwebtoken) => {
     const bufferData = await QRCode.toBuffer(jsonwebtoken);
     return bufferData;
 }
 
+const cacheAuthenticationToken = async (companyID, warehouseID, authenticationToken) => {
+    await redisClient.connect();
+    await redisClient.setEx(`${companyID}_${warehouseID}`, process.env.MASTERGST_TOKEN_EXPIRY, authenticationToken);
+}
+
 module.exports = {
-    convertJWTToBuffer
+    convertJWTToBuffer,
+    cacheAuthenticationToken
 };
